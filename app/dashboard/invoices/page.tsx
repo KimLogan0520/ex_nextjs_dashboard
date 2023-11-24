1a3d979086a792dce1aa961bb0fc28b79b1f9e65
KimLogan0520 <kimlogan0520@gmail.com>
2023-11-24 15:46:00 +0900

    ~chapter11

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,45 @@
-export default function Page() {
-    return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import {fetchInvoicesPages} from "@/app/lib/data";
+
+/**
+ * useSearchParams: 현재 URL의 매개변수에 접근할 수 있게 해줍니다. 예를 들어, URL '/dashboard/invoices?page=1&query=pending'의 경우 useSearchParams를 통해 {page: '1', query: 'pending'}과 같은 검색 매개변수를 얻을 수 있습니다.
+ * usePathname: 현재 URL의 경로명을 읽을 수 있습니다. 예를 들어, 라우트 '/dashboard/invoices'의 경우 usePathname은 '/dashboard/invoices'를 반환합니다.
+ * useRouter: 클라이언트 컴포넌트 내에서 라우트 간 탐색을 프로그래밍적으로 가능하게 합니다. 여러 가지 사용 가능한 메서드가 있습니다.
+ */
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  }
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,50 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname(); // console.log('==> pathname :', pathname); /dashboard/invoices
+  const { replace } = useRouter();
+
+  // function handleSearch(term: string) {
+  //   // console.log('*==========> input창에 입력한 검색어 :', term);
+  //   const params = new URLSearchParams(searchParams);
+  //
+  //   if ( term ) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+  //   // console.log('*==========> params by URLSearchParams:', params.get('query'));
+  //   /**
+  //    * URLSearchParams는 URL 쿼리 매개변수를 조작하기 위한 유틸리티 메서드를 제공하는 Web API입니다. 복잡한 문자열 리터럴을 생성하는 대신에 이를 사용하여 ?page=1&query=a와 같은 파라미터 문자열을 얻을 수 있습니다.
+  //    */
+  //
+  //   /**
+  //    * params.toString()에 의해서, 입력바에 유저가 입력한 검색값이 URL에 친화적인 형식으로 translate된다.
+  //    * 또한 유저가 검색어를 입력했을때, 페이지의 리로드 없이 Next.js의 클라이언트사이드 네비게이션 덕에 URL이 업데이트 된다.
+  //    */
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    // console.log('*=======> Searching :', term);
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if ( term ) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,7 +52,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+        // placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
