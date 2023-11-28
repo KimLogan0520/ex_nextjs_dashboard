22ca621129fd98b7af1f065f620a5de733520e5e
KimLogan0520 <kimlogan0520@gmail.com>
2023-11-28 17:53:31 +0900

    ~chapter14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,18 +12,33 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), // coerce : 강요하다 구속하다 지배하다 위압하다
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: '고객을 선택해 주세요.'
+  }),
+  amount: z.coerce.number().gt(0, { message: '$0 보다 큰 금액을 입력해 주세요.' }), // coerce : 강요하다 구속하다 지배하다 위압하다
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: '송장 상태를 선택해 주세요.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
   // console.log('*=======> createInvoice called!');
   // console.log('*=======> formData :', formData);
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+  // safeParse() will return an object containing either a success or error field. This will help handle validation more gracefully without having put this logic inside the try/catch block.
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
@@ -32,6 +47,14 @@ export async function createInvoice(formData: FormData) {
   // console.log('*=======> amount :', amount);
   // console.log('*=======> status :', status);
 
+  if ( !validatedFields.success ) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   // console.log('*=======> date :', date);
@@ -51,13 +74,21 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if ( !validatedFields.success ) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
